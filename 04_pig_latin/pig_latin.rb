48ebca45131dda91053b7c7bd7dc997fb66a4871
larrygold <larry@goldstein.fr>
2018-04-09 23:43:31 +0200

    larry 23h43

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,9 @@
 #write your code here
 def translate(string)
 
+
+    # Cas : plusieurs mots
+
   if string.split(' ').size > 1
     i = 0
     translation = ""
@@ -9,6 +12,9 @@ def translate(string)
       i+= 1
     end
     return translation + translate(string.split(' ')[string.split(' ').size-1])
+
+
+    # Cas : commence par une consonne suivie de "qu"
 
   elsif (string.split('')[1] + string.split('')[2] == "qu") && ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"].include?(string.split('')[0])
 
@@ -26,6 +32,7 @@ def translate(string)
     return word_without_firsts_letter + first_letter + second_letter + third_letter + "ay"
 
 
+    # Cas : commence par "qu"
 
   elsif string.split('')[0] + string.split('')[1] == "qu"
 
@@ -41,8 +48,14 @@ def translate(string)
 
     return word_without_firsts_letter + first_letter + second_letter + "ay"
 
+
+    # Cas : commence par une voyelle
+
   elsif ["a", "e", "i", "o", "u", "y"].include?(string.split('')[0])
     return string + "ay"
+
+
+    # Cas : commence par 3 consonnes
 
   elsif ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"].include?(string.split('')[1]) && ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"].include?(string.split('')[2])
 
@@ -60,6 +73,8 @@ def translate(string)
     return word_without_firsts_letter + first_letter + second_letter + third_letter + "ay"
 
 
+    # Cas : commence par 2 consonnes
+
   elsif ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"].include?(string.split('')[1])
 
     first_letter = string.split('')[0]
@@ -74,6 +89,8 @@ def translate(string)
 
     return word_without_firsts_letter + first_letter + second_letter + "ay"
 
+
+    # Cas : commence par 1 consonne
 
   else
 
